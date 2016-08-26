da0eef43d64ad804dab3ab91b8dbb86b543017fd
ilyamodder <ilyamodder@gmail.com>
2016-08-26 11:07:51 +0300

    Some improvements

--- android-app/app/src/main/java/com/nazir/myapplication123/GamePadActivity.java
+++ android-app/app/src/main/java/com/nazir/myapplication123/GamePadActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -88,6 +90,11 @@ public class GamePadActivity extends AppCompatActivity  implements OrientationSe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main2);
         ButterKnife.bind(GamePadActivity.this);
         mLast = new double[3];
